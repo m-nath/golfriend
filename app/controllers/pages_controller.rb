b7ecb18dbe41648f0fc9f819121c40aaa020c576
Christee Song <christee.song@gmail.com>
2019-08-15 08:30:54 +0900

    changed card css grid,page home controller sample number

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :show]
 
   def home
-    @events = Event.all
+    @events = Event.all.sample(9)
     @user = current_user
   end
 
