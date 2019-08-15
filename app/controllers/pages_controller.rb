ad0ba146601ab17f24752a1640e8368a43ac4be6
Christee Song <christee.song@gmail.com>
2019-08-15 13:56:01 +0900

    map edited infoWindow and img marker

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,15 +4,13 @@ class PagesController < ApplicationController
   def home
     @user = current_user
     # @events = Event.all.sample(12)
-    @events = Event.geocoded #returns events with coordinates
-
-
+    @events = Event.geocoded.sample(12) #returns events with coordinates
     @markers = @events.map do |event|
       {
         lat: event.latitude,
         lng: event.longitude,
-        infoWindow: render_to_string(partial: "info_window", locals: { event: event }),
-        image_url: helpers.asset_url('birdie.png')
+        infoWindow: render_to_string(partial: 'info_window', locals: { event: event }),
+        image_url: helpers.asset_url('birdie.png'),
       }
     end
   end
