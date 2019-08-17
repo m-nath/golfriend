81120ff15884b9ec74c9d3f3060c3aeaed553f08
m-nath <48682760+m-nath@users.noreply.github.com>
2019-08-17 12:32:09 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -50,7 +50,7 @@ huishu = User.create!(
 )
 
 10.times do
-  User.create!(email: Faker::Internet.email, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, password: "123123", photo: Faker::Avatar.image)
+  User.create!(email: Faker::Internet.email, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, password: "123123")
 end
 
 puts "added #{User.count} users."
