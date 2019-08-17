40f063f679d584ed3e075f7bf9b0807b2b24ad91
nath mongkolrathmanee <m.nath1105@gmail.com>
2019-08-17 12:26:14 +0900

    seed avatar image url

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,8 @@ puts "seed started.. for users and events.."
 
 Message.delete_all if Rails.env.development?
 Reservation.delete_all if Rails.env.development?
+Interest.delete_all if Rails.env.development?
+
 Event.delete_all if Rails.env.development?
 User.delete_all if Rails.env.development?
 
@@ -19,7 +21,7 @@ nath = User.create!(
   first_name: "Nath",
   last_name: "M",
   admin: true,
-  photo: "nath.jpg"
+  remote_photo_url: "https://res.cloudinary.com/dxouryvao/image/upload/v1566010222/nath.jpg"
 )
 
 christee = User.create!(
@@ -28,7 +30,7 @@ christee = User.create!(
   first_name: "Christee",
   last_name: "Song",
   admin: true,
-  photo: "christee.jpg"
+  remote_photo_url: "https://res.cloudinary.com/dxouryvao/image/upload/v1566010222/christee.jpg"
 )
 
 shohei = User.create!(
@@ -36,7 +38,7 @@ shohei = User.create!(
   password: "123123",
   first_name: "Shohei",
   last_name: "Okubo",
-  photo: "shohei.jpg"
+  remote_photo_url: "https://res.cloudinary.com/dxouryvao/image/upload/v1566010933/shohei.jpg"
 )
 
 huishu = User.create!(
@@ -44,7 +46,7 @@ huishu = User.create!(
   password: "123123",
   first_name: "Huishu",
   last_name: "Jia",
-  photo: "huishu.jpg"
+  remote_photo_url: "https://res.cloudinary.com/dxouryvao/image/upload/v1566010222/huishu.jpg"
 )
 
 10.times do
